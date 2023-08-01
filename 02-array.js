bf5ac59fee90e5499df5044d0f211ea385d17337
GadiegoN <ngadiego@gmail.com>
2023-08-01 17:38:09 -0300

    add challenge arrays

--- 02-array.js
+++ 02-array.js
@@ -1,11 +1,25 @@
 // 1- Crie um array vazio chamado animals
 
+let animals = [];
+
 // 2- Adicione nesse array dois animais: dog e cat (você pode fazer isso de mais de uma forma diferente!)
+
+animals.push('dog', 'cat');
 
 // 3- Adicione no começo do array outro animal: spider
 
+animals.unshift('spider');
+
 // 4- Remova o primeiro elemento do array
+
+animals.shift();
 
 // 5- Remova o último elemento do array
 
+animals.pop();
+
 // 6- Como ficou o array final?! E qual é o elemento 1 desse array?
+
+console.log(animals)
+
+// [0 : "dog"]
