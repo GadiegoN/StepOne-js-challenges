44ddaccb76449bbd66e615d703144583a6285f06
GadiegoN <ngadiego@gmail.com>
2023-08-03 11:15:19 -0300

    add challenge six

--- 06-if_else.js
+++ 06-if_else.js
@@ -1,9 +1,16 @@
 // Escreva uma função com o nome examResult que tenha note como argumento.
 
+let note = 70;
+
 // Esse função vai verificar se o valor note é maior ou igual a 75, se for deve mostrar no console a seguinte frase: 
+if (note >= 75) {
+    // "You passed the exam!"
+    console.log("You passed the exam!");
+    // Se não, deve mostrar no console a seguinte frase:
+} else {
+    // "You didn't passed the exam!"
+    console.log("You didn't pass the exam!");
+}
 
-// "You passed the exam!"
 
-// Se não, deve mostrar no console a seguinte frase:
 
-// "You didn't passed the exam!"
