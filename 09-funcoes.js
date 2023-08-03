141a9a7cfd4a59ecf0d84058716b52f9a982f743
GadiegoN <ngadiego@gmail.com>
2023-08-03 17:50:42 -0300

    add challenge none

--- 09-funcoes.js
+++ 09-funcoes.js
@@ -2,19 +2,41 @@
 
 // 1- Função de soma, com o nome ‘sum', que retorna a soma de dois números.
 
+function sum(a, b) {
 
+    let resultSum = a + b;
+    return resultSum;
+}
 
 
 // 2- Função de subtração, com o nome ‘subtract’, que retorna a subtração de dois números
 
+function subtract(a, b) {
+
+    let resultSubtract = a - b;
+    return resultSubtract;
+}
 
 
 // 3- Função de multiplicação, com o nome ‘multiplication, que retorna 
 // a multiplicação de dois números
 
+function multiplication(a, b) {
 
+    let resultMultiplication = a * b;
+    return resultMultiplication;
+}
 
 // 4- Função de divisão, com o nome 'division', que retorna a divisão do primeiro 
 //número pelo outro
 
+function division(a, b) {
 
+    let resultDivision = a / b;
+    return resultDivision;
+}
+
+console.log(sum(5, 4))
+console.log(subtract(5, 4))
+console.log(multiplication(5, 4))
+console.log(division(5, 4))
